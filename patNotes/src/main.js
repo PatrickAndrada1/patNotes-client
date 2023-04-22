f2fad76eb074ac5a79bef16ee7f0c1a58c450833
Patrick_Andrada <patrickanthonio993@gmail.com>
2023-04-22 14:23:19 +0800

    removing sweet alert pt1

--- patNotes/src/main.js
+++ patNotes/src/main.js
@@ -6,7 +6,7 @@ import router from "./router";
 import "./index.css";
 // import './assets/main.css'
 import VueSweetalert2 from "vue-sweetalert2";
-import "sweetalert2/dist/sweetalert2.min.css";
+
 
 const app = createApp(App);
 
